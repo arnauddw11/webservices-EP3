53a35862848886538da8196efc5da308749c05b2
Arnaud De Weghe <arnaud.deweghe@student.hogent.be>
2022-07-25 14:47:27 +0200

    databse connectie, getdb, initdb en closedb

--- src/data/index.js
+++ src/data/index.js
@@ -1,9 +1,45 @@
+const config = require('config');
+const mongoose = require('mongoose');
+const { env } = require('../../config/custom-environment-variables');
+const { getChildLogger } = require('../core/logging');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://arnaud:arnaud1@webservices.hbyz1zu.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+let db;
+async function initialiseDb() {
+  const logger = getChildLogger('database');
+  logger.info('Initialiseren van de databank connectie');
+
+  const uri = "mongodb+srv://arnaud:arnaud1@cluster0.kpaa7.mongodb.net/testDB?retryWrites=true&w=majority";
+  
+  logger.info(uri);
+  try {
+    mongoose.connect(
+      uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      },
+      () => {
+        logger.info(" Mongoose is verbonden")
+        db = mongoose.connection;
+
+        db.on('error', logger.info.bind(logger, 'MongoDB connectie niet geslaagd:'));
+      }
+    );
+  } catch (e) {
+    logger.info("Kan geen verbinding maken");
+  }
+};
+
+function getMongoDb() {
+  if (!db) throw new Error('Initialiseer data laag alvorens in de mongodb instance te gaan');
+  return db;
+};
+async function closeDb() {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await db.stop();
+}
+  module.exports = {
+    initialiseDb,
+    getMongoDb,
+    closeDb
+  };
