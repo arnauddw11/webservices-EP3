eeca1f6ac23ddff843cda29ba8bc5d0c1568a0a2
arnauddw11 <arnaud.deweghe@student.hogent.be>
2022-08-08 15:13:09 +0200

    Documenteren swagger

--- src/rest/_bovenstuk.js
+++ src/rest/_bovenstuk.js
@@ -4,19 +4,92 @@ const Role = require('../core/roles');
 const bovenstukService = require('../service/bovenstuk');
 const { requireAuthentication, makeRequireRole } = require('../core/auth');
 
+/**
+ * @swagger
+ * tags:
+ *   name: Bovenstuks
+ *   description: Represents a Represents a garment worn as an upper garment
+ */
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Bovenstuks:
+ *       allOf:
+ *         - $ref: "#/components/schemas/Base"
+ *         - type: object
+ *           required:
+ *             - name
+ *             - dropdate
+ *             - size
+ *           properties:
+ *             name:
+ *               type: "string"
+ *             dropdate:
+ *               type: "string"
+ *               format: date-time
+ *             size:
+ *               type: "string"
+ *           example:
+ *             $ref: "#/components/examples/Bovenstuk"
+ *   examples:
+ *     Bovenstuk:
+ *       _id: "62f0ef08c42d98f6f92358e3"
+ *       name: "Adidas performance tshirt"
+ *       dropdate: "2021-05-28T14:27:32.534Z"
+ *       size: "M"
+ */
 
+/**
+ * @swagger
+ * /api/bovenstuk:
+ *   get:
+ *     summary: Get all bovenstuks
+ *     tags:
+ *     - Bovenstuk
+ */
 const getAllBovenstukken = async (ctx) => {
   ctx.body = await bovenstukService.getAll();
 };
-
+/**
+ * @swagger
+ * /api/bovenstuk:
+ *   get:
+ *     summary: Get bovenstuk by Name
+ *     tags:
+ *     - Bovenstuk
+ */
 const getBovenstukByName = async (ctx) => {
   ctx.body = await bovenstukService.getByName(ctx.params.name);
 };
-
+/**
+ * @swagger
+ * /api/bovenstuk:
+ *   get:
+ *     summary: Get bovenstuk by id
+ *     tags:
+ *     - Bovenstuk
+ */
 const getBovenstukById = async (ctx) => {
   ctx.body = await bovenstukService.getById(ctx.params.id);
 };
-
+/**
+ * @swagger
+ * /api/bovenstuk:
+ *   post:
+ *     summary: Create a new bovenstuk
+ *     description: Creates a new bovenstuk for the signed in user.
+ *     tags:
+ *      - Bovenstuk
+ 
+ *     responses:
+ *       200:
+ *         description: The created bovenstuk
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Bovenstuk"
+ * */
 const createBovenstuk = async (ctx) => {
   const newBovenstuk = await bovenstukService.create({
     ...ctx.request.body,
@@ -25,7 +98,14 @@ const createBovenstuk = async (ctx) => {
   ctx.status=201;
 };
 
-
+/**
+ * @swagger
+ * /api/bovenstuk/size/:size:
+ *   get:
+ *     summary: Get bovenstuk by size
+ *     tags:
+ *     - Bovenstuk
+ */
 const getBovenstukBySize = async (ctx) => {
   ctx.body = await bovenstukService.getBySize(ctx.params.size);
 };
--- src/rest/index.js
+++ src/rest/index.js
@@ -7,7 +7,40 @@ const installSchoenRouter = require('./_schoen');
 const installOutfitRouter = require('./_outfit');
 const installUserRouter = require('./_user');
 const installValidationRouter = require('./_validation');
-
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Base:
+ *       required:
+ *         - _id
+  *       properties:
+ *         _id:
+ *           type: string
+ *           format: "ObjectId"
+ *       example:
+ *         _id: "62f0ef08c42d98f6f92358e3"
+ *     
+ *     User:
+ *       allOf:
+ *         - $ref: "#/components/schemas/Base"
+ *         - type: object
+ *           required:
+ *             - name
+ *           properties:
+ *             name:
+ *               type: "string"
+ *             email:
+ *               type: "string"
+ *               format: email
+ *           example:
+ *             $ref: "#/components/examples/User"
+ *   examples:
+ *     User:
+ *       _id: "62f0ef08c42d98f6f92358e3"
+ *       name: "Arnaud dw"
+ *       email: "arnaud.dw@kledingapp.be"
+ */
 
 /**
  * Install all routes in the given Koa application.
